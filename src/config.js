1be7cc0fd8b96423b8b81f9365d9567c70bd38e7
Elon levi <elonlevi2@gmail.com>
2023-07-24 17:29:31 +0300

    h

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
 import { toast } from 'react-toastify';
 
 
-export const localhost = 'http://localhost:8000/api/'
-// export const localhost = 'http://18.194.173.51/django/api/'
+// export const localhost = 'http://localhost:8000/api/'
+export const localhost = 'http://18.194.173.51/django/api/'
 export const s3Url = 'https://nadlans3.s3.eu-central-1.amazonaws.com/properties/'
 
 
